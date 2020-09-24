a165d37e7988be3e4c64b804805b809f74d8da6e
Administrator <Administrator@10.15.205.6>
2020-09-24 17:10:22 +0800

    eudic v1 finished

--- Z4/src/main/java/com/jin/eudic/MainRunner.java
+++ Z4/src/main/java/com/jin/eudic/MainRunner.java
@@ -1,19 +1,23 @@
 package com.jin.eudic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class MainRunner {
     public static void main(String[] args) {
         EudicOpt eo = new EudicOpt();
         eo.setLoadCategoryMap(true);eo.setLoadAllCatFromWeb(false);
         eo.setWriteCatListFile(false);eo.setWriteCatWordListFile(false);
-        eo.init();
+		eo.init();
+		String note = eo.findNoteByWord("slightly");
+		System.out.println(eo.updateWordNote("slightly", note));
+		//System.out.println(eo.getCategoryMap());
         //eo.queryAllCategory();
         //System.out.println(eo.findCatsForWordFromFile("striking"));
-        List<String> wordList = new ArrayList<String>();
-	wordList.add("fuck");wordList.add("shit");
-	eo.moveWordFromCat("0301B", "temp（默认）Jin", wordList);
+		/*
+		 * List<String> wordList = new ArrayList<String>();
+		 * wordList.add("fuck");wordList.add("shit"); eo.moveWordFromCat("0301B",
+		 * "temp（默认）Jin", wordList);
+		 */
         //System.out.println(eo.delWordFromCat("temp（默认）Jin", wordList));
         //eo.queryAllCategory(true);
         //System.out.println(eo.findCategoriesForWord("striking", true));
