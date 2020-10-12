6ed37703ece12a40c0121b5daebfb3f8dad72e18
jin-dev <jwang@salmon.com>
2020-10-12 13:06:49 +0800

    FT

--- Z4/src/main/java/com/jin/ds/recursive/FileTree.java
+++ Z4/src/main/java/com/jin/ds/recursive/FileTree.java
@@ -1,37 +1,40 @@
 package com.jin.ds.recursive;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 public class FileTree {
     public static final String SPACE = "  ";
     public static final String ROOT_LOCATION = "e:\\Install_Files";
+
     public static void main(String[] args) {
-	new FileTree().displayNoRecur(new File(ROOT_LOCATION));
+
+        FileTree fileTree = new FileTree();
+        fileTree.displayNoRecur(new File(ROOT_LOCATION));
+        System.out.println("------------------------------------------------------------------------");
+        fileTree.displayRecur(new File(ROOT_LOCATION), 0);
     }
-    
+
     public void displayRecur(File f, int level) {
-	String prefix="";
-	for(int i=0;i<level;i++) {
-	    prefix+=SPACE;
-	}
-	String fileName = f.getName();
-	if(f.isDirectory()) {
-	    fileName = "+"+fileName;
-	    System.out.println(prefix+fileName);
-	    File fArr[] = f.listFiles();
-	    level++;
-	    for (int i = 0; i < fArr.length; i++) {
-		displayRecur(fArr[i], level);
-	    }
-	} else {
-	    fileName = "-"+fileName;
-	    System.out.println(prefix+fileName);
-	}
+        String prefix = "";
+        for (int i = 0; i < level; i++) {
+            prefix += SPACE;
+        }
+        String fileName = f.getName();
+        if (f.isDirectory()) {
+            fileName = "+" + fileName;
+            System.out.println(prefix + fileName);
+            File fArr[] = f.listFiles();
+            level++;
+            for (int i = 0; i < fArr.length; i++) {
+                displayRecur(fArr[i], level);
+            }
+        } else {
+            fileName = "-" + fileName;
+            System.out.println(prefix + fileName);
+        }
     }
-    
+
     public void displayNoRecur(File file) {
 	/*
 +Install_Files
@@ -44,33 +47,33 @@ public class FileTree {
   -ultraedit.zip
   -ZoomInstaller.exe
 	 */
-	Stack<File> s = new Stack<File>();
-	Stack<Integer> levelStack = new Stack<Integer>();
-	s.push(file);
-	int level = 0;
-	levelStack.push(level);
-	while(!s.empty()) {
-	    String prefix = "";
-	    level = levelStack.pop();
-	    for (int j = 0; j < level; j++) {
-		prefix += SPACE;
-	    }
-	    file = s.pop();
-	    String fileName = file.getName();
-	    if (file.isDirectory()) {
-		fileName="+"+fileName;
-		File fArr[] = file.listFiles();
-		level++;
-		for (int i = 0; i < fArr.length; i++) {
-		    file = fArr[i];
-		    s.push(file);
-		    levelStack.push(level);
-		}
-	    } else {
-		fileName="-"+fileName;
-	    }
-	    System.out.println(prefix + fileName);
-	}
-	
+        Stack<File> s = new Stack<File>();
+        Stack<Integer> levelStack = new Stack<Integer>();
+        s.push(file);
+        int level = 0;
+        levelStack.push(level);
+        while (!s.empty()) {
+            String prefix = "";
+            level = levelStack.pop();
+            for (int j = 0; j < level; j++) {
+                prefix += SPACE;
+            }
+            file = s.pop();
+            String fileName = file.getName();
+            if (file.isDirectory()) {
+                fileName = "+" + fileName;
+                File fArr[] = file.listFiles();
+                level++;
+                for (int i = 0; i < fArr.length; i++) {
+                    file = fArr[i];
+                    s.push(file);
+                    levelStack.push(level);
+                }
+            } else {
+                fileName = "-" + fileName;
+            }
+            System.out.println(prefix + fileName);
+        }
+
     }
 }
