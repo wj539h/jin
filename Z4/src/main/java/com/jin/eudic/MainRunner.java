277fd63f175ffabd7e4a53d8fed67ac8848d1d07
Administrator <Administrator@wjacer>
2020-10-12 09:29:36 +0800

    National holiday Update

--- Z4/src/main/java/com/jin/eudic/MainRunner.java
+++ Z4/src/main/java/com/jin/eudic/MainRunner.java
@@ -1,22 +1,66 @@
 package com.jin.eudic;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class MainRunner {
-    public static void main(String[] args) throws Exception{
-        EudicOpt eo = new EudicOpt();
-        eo.setLoadCategoryMap(true);eo.setLoadAllCatFromWeb(false);
-        eo.setWriteCatListFile(false);eo.setWriteCatWordListFile(false);
+	public static void main(String[] args) throws Exception {
+
+		EudicOpt eo = new EudicOpt();
+		eo.setLoadCategoryMap(true);
+		eo.setLoadAllCatFromWeb(false);
+		eo.setWriteCatListFile(false);
+		eo.setWriteCatWordListFile(false);
 		eo.init();
 		
-		//String note = eo.findNoteByWord("dog");
-		Map<String,String> map = eo.findWordsAndNoteByCat("0109B");
+		
+    	
+		//查询某一个单词的note
+		/*String word = "dog";
+		String note = eo.findNoteByWord(word);
+		System.out.println(note);
+		
+		//更新一个单词的note
+		note+="hahaha";
+		System.out.println(eo.updateWordNote(word, note));*/
+		
+		
+		//查询Category里面的词和note
+		/*
+		String catName = "0301A";
+		Map<String,String> map = eo.findWordsAndNoteByCat(catName);
 		for (Map.Entry<String, String> me : map.entrySet()) {
-			System.out.println(me.getKey()+"  :  "+me.getValue());
+			System.out.println(me.getKey()+EudicOpt.STR_LINE_JOINNER+me.getValue());
+		}*/
+		
+		
+		
+		//从temp文件加载单词和note到map,然后导入到eudic里
+		/*String catName = "0301B";
+		Map<String,String> map = eo.loadWordNoteFromFile();
+		List<String> wordList = new LinkedList<String>();
+		int n=0;
+		for (Map.Entry<String,String> me : map.entrySet()) {
+			String word = null;
+			if((word = me.getKey()) != null) {
+				wordList.add(word);
+				String oriNote = eo.findNoteByWord(word);
+				String double_crlf = StringUtils.isEmpty(oriNote)?"":EudicOpt.STR_CRLF+EudicOpt.STR_CRLF; 
+				String newNote = me.getValue();
+				System.out.println(++n+" -- "+me.getKey()+" -- "+eo.updateWordNote(me.getKey(), oriNote+double_crlf+newNote));
+			}
 		}
+		eo.addWordToCat(catName, wordList);*/
+		
+		
+		//列出单词所在的Category
+		System.out.println(eo.findCatsForWord("shift"));
+		
+		
 		//System.out.println(eo.updateWordNote("slightly", note));
 		//System.out.println(eo.getCategoryMap());
         //eo.queryAllCategory();
@@ -33,3 +77,7 @@ public class MainRunner {
         //eo.outToFile("ffffff");
     }
 }
+
+
+
+
