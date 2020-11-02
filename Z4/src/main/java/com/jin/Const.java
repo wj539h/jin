39caac4362018e802cb28908cf0eddaf6c5cfee9
Administrator <Administrator@wjacer>
2020-11-02 23:05:25 +0800

    eudic+keke

--- Z4/src/main/java/com/jin/Const.java
+++ Z4/src/main/java/com/jin/Const.java
@@ -1,8 +1,24 @@
 package com.jin;
 
-import cn.hutool.core.io.file.FileReader;
+import static com.jin.eudic.EudicConst.CRLF;
+import static com.jin.eudic.EudicConst.FILE_DIR_NOTE_TEMP;
+import static com.jin.eudic.EudicConst.LINE_JOINNER;
+import static com.jin.eudic.EudicConst.UTF8;
 
-import static com.jin.eudic.EudicConst.FILE_DIR_CAT_LIST;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.collections4.map.ListOrderedMap;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import cn.hutool.core.io.file.FileReader;
 
 public class Const {
     public static final String full_sap = "-----------------------------------------------------------------------------";
@@ -10,11 +26,10 @@ public class Const {
     public static void pln(){System.out.println();}
     public static void pln(Object content){System.out.println(content);}
     public static void p(Object content){System.out.print(content);}
-
+    public static final String CR = "\r";
+    public static final String LF = "\n";
+    public static final String LINE_JOINNER = "||";
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader("D:\\idea_workspace\\jin\\Z4\\"+FILE_DIR_CAT_LIST);
-        String result = fileReader.readString();
-        pln(result);
     }
 }
 
--- Z4/src/main/java/com/jin/eudic/ReadKeke.java
+++ Z4/src/main/java/com/jin/eudic/ReadKeke.java
@@ -1,9 +1,14 @@
 package com.jin.eudic;
 
 import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.http.HtmlUtil;
 import cn.hutool.http.HttpUtil;
-import org.apache.commons.io.FileUtils;
 
+import org.apache.commons.collections4.map.ListOrderedMap;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import static com.jin.eudic.EudicConst.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -14,25 +19,41 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReadKeke {
-	public void spiderWeb() {
-		Pattern p = Pattern.compile("http.*?daxue/.*shtml.*第?册:U[0-9]{1,2}[A|B]");
-		for(int i=1;i<=74;i++) {
-			String r2= HttpUtil.get("http://www.kekenet.com/daxue/16522/List_"+i+".shtml", CharsetUtil.CHARSET_UTF_8);
+	public List<String> spiderWebLink() {
+		List<String> result = new ArrayList<String>();
+		Pattern p = Pattern.compile("http.*?daxue/.*shtml.*第?册:U[0-9]{1,2}[A|B]{0,1}");
+		for(int i=1;i<=75;i++) {
+			String url = i==75?"http://www.kekenet.com/daxue/16522":"http://www.kekenet.com/daxue/16522/List_"+i+".shtml";
+			String r2= HttpUtil.get(url, CharsetUtil.CHARSET_UTF_8);
 			Matcher m = p.matcher(r2);
 			int s = 0;
 			int e = 0;
 			while(m.find()) {
 				s = m.start();e=m.end();
-				System.out.println(r2.substring(s, e));
+				result.add(r2.substring(s, e)+CRLF);
 			}
 		}
+		//System.out.println(result);
+		return result;
 	}
 	
-	public void generateCatalog() throws IOException {
-		List<String> l = FileUtils.readLines(new File("d:/rb"), "utf-8");
-    	Map<String,List<String>> map = new HashMap<String,List<String>>();
+	public String spiderText(String url) {
+		String str= HttpUtil.get(url, CharsetUtil.CHARSET_UTF_8);
+		int s = str.indexOf("<div class=\"info-qh\">");
+		int e = str.indexOf("<span style=\"display:none\" id=\"source\">");
+		str = str.substring(s, e);
+		str = HtmlUtil.removeHtmlTag(str,"strong");
+		str = HtmlUtil.filter(str);
+		str = HtmlUtil.unescape(str);
+		//System.out.println(HtmlUtil.removeHtmlTag(str,"strong"));
+		return str;
+	}
+	
+	public List<String> generateCatalog() throws IOException {
+		List<String> l = spiderWebLink();
 		//String str = "http://www.kekenet.com/daxue/201612/482892.shtml\" title=\"现代大学英语精读(第2版)第二册:U1A Another School Year—What For?(2)\" target=\"_blank\">现代大学英语精读(第2版)第二册:U1A";
-		Pattern p = Pattern.compile("(h.*?shtml)(.*第)(.*册):(U[0-9]{1,2}[A|B].*target=)");
+		Map<String,List<String>> map = new HashMap<String,List<String>>();
+		Pattern p = Pattern.compile("(h.*?shtml)(.*第)(.*册):(U[0-9]{1,2}[A|B]{0,1}.*target=)");
 		for (String line : l) {
 			Matcher m = p.matcher(line);
 			m.find();
@@ -56,17 +77,32 @@ public class ReadKeke {
 							book = "03";
 						}else if(v.equals("四册")) {
 							book = "04";
+						}else if(v.equals("五册")) {
+							book = "05";
+						}else if(v.equals("六册")) {
+							book = "06";
 						}
 						break;
 					case 4 :
 						int begin = 0;
 						String arr[] = v.split(" ");
-						if(arr[0].length()==3) {
-							begin = 4;
-							key = book+arr[0].replace("U", "0");
-						}else if(arr[0].length()==4) {
-							begin = 5;
-							key = book+arr[0].replace("U", "");
+						String unit = arr[0];
+						if(unit.endsWith("A") || unit.endsWith("B")) {
+							if(unit.length()==3) {
+								begin = 4;
+								key = book+unit.replace("U", "0");
+							}else if(unit.length()==4) {
+								begin = 5;
+								key = book+unit.replace("U", "");
+							}
+						}else {
+							if(unit.length()==2) {
+								begin = 3;
+								key = book+unit.replace("U", "0");
+							}else if(unit.length()==3) {
+								begin = 4;
+								key = book+unit.replace("U", "");
+							}
 						}
 						list = map.get(key) == null? new ArrayList<String>() : map.get(key);
 						int end = v.indexOf("\"");
@@ -79,35 +115,79 @@ public class ReadKeke {
 			}
 		}
 		
-		System.out.println(map);
+		//System.out.println(map);
 		Set<Map.Entry<String, List<String>>> set = map.entrySet();
 		Iterator<Map.Entry<String, List<String>>> it = set.iterator();
+		l = new ArrayList<String>();
 		while(it.hasNext()) {
+			String v = "";
 			Map.Entry<String, List<String>> me = it.next();
-			System.out.println(me.getKey());
-			System.out.println();
+			l.add(me.getKey());
 			List<String> list = me.getValue();
 			for (String s : list) {
-				System.out.println(s);
+				l.add(s);
 			}
-			System.out.println();
-			System.out.println("--------------------------------------------------------");
+			l.add("--------------");
+		}
+		return l;
+	}
+	
+	public void sort() throws IOException {
+		List<String> l = generateCatalog();
+		Pattern p = Pattern.compile("\\d{4}[A|B]{0,1}");
+        ListOrderedMap<String, List<String>> map = new ListOrderedMap<String, List<String>>();
+        List<String> list = null;
+        String catalog = null;
+		for(String s:l) {
+			Matcher m = p.matcher(s);
+			//m.region(0, 5);
+			if(m.matches()) {
+				list = map.get(s) == null?new ArrayList<String>():map.get(s);
+				catalog = s;
+			} else if(StringUtils.isNotEmpty(s) && !StringUtils.startsWith(s, "---")) {
+				list.add(s);
+			} else if(StringUtils.startsWith(s, "---")) {
+				map.put(catalog, list);
+			}
+		}
+		Object objArr[] = map.keyList().toArray();
+		Arrays.sort(objArr);
+		ListOrderedMap<String, List<String>> newMap = new ListOrderedMap<String, List<String>>();
+		for (int i = 0; i < objArr.length; i++) {
+			String cata = (String)objArr[i];
+			list = map.get(cata);
+			String tmpA[] = new String[30];
+			for(String str : list) {
+				String ta[] = str.split(" \\| ");
+				String title = ta[1].substring(ta[1].lastIndexOf("(")+1, ta[1].length()-1);
+				tmpA[Integer.valueOf(title)-1] = str;
+			}
+			newMap.put(cata,Arrays.asList(tmpA));
+		}
+		
+		for (Map.Entry<String, List<String>> me : newMap.entrySet()) {
+			String content = me.getKey()+CRLF;
+			String text = "";
+			List<String> l1 = me.getValue();
+			for(String str : l1) {
+				if(str!=null) {
+					String ta[] = str.split(" \\| ");
+					//System.out.println(ta[1]+" | "+ta[0]);
+					content+=ta[1]+" | "+ta[0]+CRLF;
+					text+=spiderText(ta[0])+CRLF;
+				}
+			}
+			content+=text+CRLF;
+			System.out.println("---------------------------------------------------------------");
+			FileUtils.writeStringToFile(new File("d:/rb1"), content, "utf-8", true);
 		}
 	}
 
 	public static void main(String[] args) {
-		String str="123&中文";
-		String utf8="UTF-8";
-		String gbk="gbk";
-		String result = null;
+		ReadKeke r = new ReadKeke();
 		try {
-			result = URLEncoder.encode(str, utf8);
-			System.out.println("str encode utf8 = " + result);
-			result = URLEncoder.encode(str, gbk);
-			System.out.println("str encode gbk = " + result);
-			result = URLDecoder.decode(str, gbk);
-			System.out.println("str dncode = " + result);
-		} catch (UnsupportedEncodingException e) {
+			r.sort();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
