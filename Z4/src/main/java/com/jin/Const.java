156b98bbd3f60788db061d9ee5ca195498159ab6
jin-dev <jwang@salmon.com>
2020-11-03 10:43:06 +0800

    a

--- Z4/src/main/java/com/jin/Const.java
+++ Z4/src/main/java/com/jin/Const.java
@@ -1,25 +1,5 @@
 package com.jin;
 
-import static com.jin.eudic.EudicConst.CRLF;
-import static com.jin.eudic.EudicConst.FILE_DIR_NOTE_TEMP;
-import static com.jin.eudic.EudicConst.LINE_JOINNER;
-import static com.jin.eudic.EudicConst.UTF8;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.collections4.map.ListOrderedMap;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import cn.hutool.core.io.file.FileReader;
-
 public class Const {
     public static final String full_sap = "-----------------------------------------------------------------------------";
     public static final String half_sap = "----------------------------------";
